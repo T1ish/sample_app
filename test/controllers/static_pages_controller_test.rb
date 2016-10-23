43ad0583b9ae38896b00a9baf22a8d2cd79facb5
T1ish <hisuiismine@yahoo.com>
2016-10-23 11:57:10 +0200

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,56 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get about" do
+  	get static_pages_about_url
+  	assert_response :success
+  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+  	get static_pages_contact_url
+  	assert_response :success
+  	assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+  	get root_url
+  	assert_response :success
+  end
+
+=begin  
+#Another way to do it instead of having redundant text is 
+#by using variales for the text and use it in the tests.
+
+  def setup
+  	@base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+  	get static_pages_about_url
+  	assert_response :success
+  	assert_select "title", "About | #{@base_title}"
+  end
+=end
 end
