8d535c6759905e292c40f136aa580172ab587472
T1ish <hisuiismine@yahoo.com>
2016-11-05 12:03:45 +0100

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,30 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
+=begin 
+	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in user
+  		params[:session][:remember_me] == '1' ? remember(user) : forget(user) # With ternary operators you cannot use remember user but have to user remember(user).
   		redirect_to user  # user is same as user_url(user) here.
+=end
+	# The above code is with the local variable.
+	# We are adding @ infront of users to make it to an instance variable instead of a local 
+	# variable as to access the cookie through test so it's not virtual cookie anymore.
+	# This also allows us to use assigns in the test "login with remembering" to check the cookie.
+	@user = User.find_by(email: params[:session][:email].downcase)
+  	if @user && @user.authenticate(params[:session][:password])
+  		log_in @user
+  		params[:session][:remember_me] == '1' ? remember(@user) : forget(@user) # With ternary operators you cannot use remember user but have to user remember(user).
+  		redirect_to @user  # user is same as user_url(user) here.
   	else
-  		flash.now[:danger] = "Invalid email/password combination" #Not quite right!
+  		flash.now[:danger] = "Invalid email/password combination" 
   		render 'new'
   	end
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 
